f075412db7f29e6566c2057219e64a614d02a122
Jordan Simmons <jordmsimm@gmail.com>
2017-09-09 19:12:05 -0500

    iterate assignment

---
+++ assignments/iterate.py
@@ -0,0 +1,34 @@
+"""Usage:
+    iterate.py [-h] [NUM] ...
+
+Arguments:
+  NUM        Number to iterate
+Options:
+  -h --help  Shows these options
+
+"""
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+
+
+
+
+
+def iterate(n):
+    for i in range(1,n+1):
+        print(i)
+        if i%3 == 0 and i%5==0:
+            print(str(i) + ' - multiple of 3 and 5')
+        elif i%3 == 0:
+            print(str(i)+ ' - multiple of 3')
+        elif i%5 == 0:
+            print(str(i) +' - multiple of 5')
+        else:
+            print(i)
+
+n = int(args['NUM'][0])
+iterate(n)
