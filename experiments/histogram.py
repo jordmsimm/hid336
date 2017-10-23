10ba10f8d9b10fa2ce06ed9306e53c3125288d4b
jordmsimm <31229826+jordmsimm@users.noreply.github.com>
2017-10-23 09:10:57 -0500

    Add Axis

--- experiments/histogram.py
+++ experiments/histogram.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 def getHist(path):
     data = pd.read_csv(path, sep= " ")
-    plot = data.hist()
+    counts = pd.value_counts(data['Student_Data'])
+    plot = counts.plot(kind='bar', title='Time Student Spent On Corections')
+    plot.set_xlabel('Time spent for corrections')
+    plot.set_ylabel('NUmber of students')
     return plot
 
 getHist('histdata.txt')
